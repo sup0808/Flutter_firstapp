2815c2c53b36cf60e2072919860af2304f88ddb0
supriya gupta <supriyag75@gmail.com>
2024-06-10 15:08:46 +0530

    Example of insertAll(index, value)

--- lib/ListExample.dart
+++ lib/ListExample.dart
@@ -9,13 +9,13 @@ void main() {
   nameList.add("Supriya");
   nameList.add("Gupta");
   nameList.add("Arvin");
-  nameList.addAll(list);
-  nameList.insert(3, "23");
+ // nameList.addAll(list);
+  nameList.insertAll(1, list);
 
   print("NameList $nameList");
 }
 
 /*
 [10, 20, 30, 40, 50]
-NameList [Supriya, Gupta, Arvin, 23, 10, 20, 30, 40, 50]
+NameList [Supriya, 10, 20, 30, 40, 50, Gupta, Arvin]
 * */
