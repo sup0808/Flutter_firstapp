72d0747cfbded8ae350e9f63a09555135f0b1736
supriya gupta <supriyag75@gmail.com>
2024-06-10 16:04:35 +0530

    ANother vay of assigning value to map at dynmically

--- lib/MapExample.dart
+++ lib/MapExample.dart
@@ -1,13 +1,16 @@
 void main() {
-  var map_name = {
+ /* var map_name = {
     'Name': "Supriya",
     'yearOfExperience' : 2,
     'Avg.rating' : true,
     'location' : 3.4
 
-  };
-
+  };*/
+  var map_name = Map();
   map_name['Name'] = "Arvind";
+  map_name['yearOfExperience'] = "Arvind";
+  map_name['rating'] = "Arvind";
+  map_name['location'] = "Arvind";
   print(map_name['key2']);
   print(map_name);
 
