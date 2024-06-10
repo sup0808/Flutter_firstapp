358c89d27e30bd29e924ff05312b631424b51b8c
supriya gupta <supriyag75@gmail.com>
2024-06-10 15:58:06 +0530

    Map example (get value based on key)

--- lib/MapExample.dart
+++ lib/MapExample.dart
@@ -6,8 +6,15 @@ void main() {
     'key4' : 3.4
 
   };
-  print(map_name);
+  print(map_name['key2']);
+  print(map_name['Key2']);
+  print(map_name['key5']);
+
 }
 
 /*
-{key1: Supriya, key2: 2, key3: true, key4: 3.4}*/
+* output
+2
+null
+null
+* */
