14f13e38b1ec41e6ae8c47a558d392011d1650e7
supriya gupta <supriyag75@gmail.com>
2024-06-10 14:00:22 +0530

    variable declaration

---
+++ lib/HumanClassExample.dart
@@ -0,0 +1,21 @@
+
+void main(){
+  //Inline Declaration
+  int b = 9;
+  String str ="";
+  bool s =true;
+
+  BigInt bigValue;
+  bigValue = BigInt.parse('11111111111233333333999');
+
+  double point = 3.4;
+
+  int? a; //Declaration of variable using nullable
+  print(a);
+}
+class User{
+
+  //Declaration of variable
+
+
+}
