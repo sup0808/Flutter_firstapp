4ca8cbb1014900100251f3a024b6abff94ce2a88
supriya gupta <supriyag75@gmail.com>
2024-06-10 16:48:55 +0530

    const anf final difference example

---
+++ lib/ConstAndFinalDiff.dart
@@ -0,0 +1,20 @@
+void main(){
+  final list1 = [
+    "supriya",
+    "Arvind",
+    "shresth"
+  ];
+
+  list1.add("ghh");
+
+  const list2 = [
+    "supya",
+    "Arvd",
+    "shrth"
+  ];
+
+  list2.add("ghh");//give exception
+  print(list2);
+
+
+}
