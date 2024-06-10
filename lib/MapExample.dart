d77e73f5b52eb31f997e5431930f5198fcbc6f61
supriya gupta <supriyag75@gmail.com>
2024-06-10 15:55:41 +0530

    Map example

---
+++ lib/MapExample.dart
@@ -0,0 +1,13 @@
+void main() {
+  var map_name = {
+    'key1': "Supriya",
+    'key2' : 2,
+    'key3' : true,
+    'key4' : 3.4
+
+  };
+  print(map_name);
+}
+
+/*
+{key1: Supriya, key2: 2, key3: true, key4: 3.4}*/
