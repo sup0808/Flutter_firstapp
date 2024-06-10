1bae1a33f5a78f387f17052d443fb279c8d8950f
supriya gupta <supriyag75@gmail.com>
2024-06-10 16:09:17 +0530

    Map function (keys,values, containsKey)

--- lib/MapExample.dart
+++ lib/MapExample.dart
@@ -11,13 +11,25 @@ void main() {
   map_name['yearOfExperience'] = "Arvind";
   map_name['rating'] = "Arvind";
   map_name['location'] = "Arvind";
-  print(map_name['key2']);
+
   print(map_name);
+  print("Length-- ${map_name.length}");
+  print("keys-- ${map_name.keys}");
+  print("values-- ${map_name.values}");
+  print("containsKey-- ${map_name.containsKey('Name')}");
+  print("isEmpty-- ${map_name.isEmpty}");
+  print("isNotEmpty-- ${map_name.isNotEmpty}");
 
 }
 
 /*
-* output :: Assi
-null
-{Name: Arvind, yearOfExperience: 2, Avg.rating: true, location: 3.4}
-* */
+* output ::{Name: Arvind, yearOfExperience: Arvind, rating: Arvind, location: Arvind}
+Length-- 4
+keys-- (Name, yearOfExperience, rating, location)
+values-- (Arvind, Arvind, Arvind, Arvind)
+containsKey-- true
+isEmpty-- false
+isNotEmpty-- true
+
+Process finished with exit code 0
+
