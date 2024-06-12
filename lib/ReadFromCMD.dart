551fc5ef382343601da720748552c5ae1501f494
supriya gupta <supriyag75@gmail.com>
2024-06-12 12:41:49 +0530

    Initial commit

--- lib/ReadFromCMD.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-void main(){
-  print("welcome to Dirt");
-  stdout.write("Enter the name"); // for printing
-  var name = stdin.readLineSync(); // for taking value from commond line
-  print("Welcome $name");
-}
