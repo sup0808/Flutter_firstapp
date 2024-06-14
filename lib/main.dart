9f299f7c2dd3c838103a82ab0a2b00c7ec952d82
supriya gupta <supriyag75@gmail.com>
2024-06-14 13:19:02 +0530

    Get Current Date and Time

--- lib/main.dart
+++ lib/main.dart
@@ -16,14 +16,16 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-        textTheme: TextTheme(
-          headlineLarge: TextStyle(fontFamily: 'FontRoboto', fontSize: 24),
-          headlineMedium:  TextStyle(fontWeight: FontWeight.w400, fontSize: 16),
-          headlineSmall:  TextStyle(fontWeight: FontWeight.w200, fontSize: 12)
-        )
+          primarySwatch: Colors.blue,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+          textTheme: TextTheme(
+              headlineLarge: TextStyle(fontFamily: 'FontRoboto', fontSize: 24),
+              headlineMedium: TextStyle(
+                  fontWeight: FontWeight.w400, fontSize: 16),
+              headlineSmall: TextStyle(
+                  fontWeight: FontWeight.w200, fontSize: 12)
+          )
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -41,12 +43,46 @@ class MyHomePage extends StatefulWidget {
 */
 
   @override
-  State<MyHomePage> createState() => _MyCustomFontFont();
+  State<MyHomePage> createState() => _DateTimePage();
 
 }
 
-class _MyCustomFontFont extends State<MyHomePage>{
-  var userName  = TextEditingController();
+class _DateTimePage extends State<MyHomePage> {
+  var time = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+      body: Center(
+        child: Container(
+          width: 300,
+          height: 300,
+          child: Column(
+            children: [
+              Text(
+                "Current Date and time : ${time}",
+                style: TextStyle(fontSize: 20),),
+              ElevatedButton(onPressed: () {
+                setState(() {
+
+                });
+              },
+                  child: Text("Get Time"))
+            ],
+          ),
+        ),
+      )
+      ,
+    );
+  }
+
+}
+
+class _MyCustomFontFont extends State<MyHomePage> {
+  var userName = TextEditingController();
   var email = TextEditingController();
 
   @override
@@ -63,7 +99,7 @@ class _MyCustomFontFont extends State<MyHomePage>{
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               TextField(
-                controller:userName ,
+                controller: userName,
                 decoration: InputDecoration(
                     hintText: "Enter Username",
                     border: OutlineInputBorder(
@@ -109,14 +145,14 @@ class _MyCustomFontFont extends State<MyHomePage>{
                     hintText: "Enter Password",
                     //suffixText: "Username exits",
                     suffixIcon: Icon(Icons.remove_red_eye, color: Colors.green,)
-                //  suffixIcon: IconButton(icon: Icons.e_mobiledata,onPressed: (){},)
+                  //  suffixIcon: IconButton(icon: Icons.e_mobiledata,onPressed: (){},)
                 ),
               ),
-              ElevatedButton(onPressed: (){
-                String nameTxt =  userName.text.toString();
+              ElevatedButton(onPressed: () {
+                String nameTxt = userName.text.toString();
                 print("UserName :: $nameTxt");
 
-                String emailTxt =  email.text.toString();
+                String emailTxt = email.text.toString();
                 print("Email :: $emailTxt");
               }, child: Text("Submit"),)
             ],
@@ -133,49 +169,62 @@ class _MyCustomFontFont extends State<MyHomePage>{
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-
-    var arrNames = ["supriya","Shresth","Arvind","One","Two","Neha","Garima"];
+    var arrNames = [
+      "supriya",
+      "Shresth",
+      "Arvind",
+      "One",
+      "Two",
+      "Neha",
+      "Garima"
+    ];
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text("Flutter Container"),
       ),
-      body: ListView.separated(itemBuilder: (context, index) {
-        return Card(
-          shadowColor: Colors.green ,
-          elevation: 10,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ListTile(
-              leading: CircleAvatar(
-                maxRadius: 50,
-                child: Container(
-                  height: 50,
-                  width: 50,
-                  child: Column(
-                    children: [
-                      Container(
-                        height: 40,
-                        width: 40,
-                        child: Image.asset('assets/images/user.jpg'),
-                      ),
-                      //Text("supr", style: TextStyle(color: Colors.black,))
-                    ],
+      body: ListView.separated(
+        itemBuilder: (context, index) {
+          return Card(
+            shadowColor: Colors.green,
+            elevation: 10,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                leading: CircleAvatar(
+                  maxRadius: 50,
+                  child: Container(
+                    height: 50,
+                    width: 50,
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 40,
+                          width: 40,
+                          child: Image.asset('assets/images/user.jpg'),
+                        ),
+                        //Text("supr", style: TextStyle(color: Colors.black,))
+                      ],
+                    ),
                   ),
                 ),
+                title: Text(arrNames[index], style: Theme
+                    .of(context)
+                    .textTheme
+                    .headlineLarge!
+                    .copyWith(color: Colors.blue)),
+                subtitle: Text('Delhi', style: basicTextStyle(),),
+                trailing: Icon(Icons.add),
               ),
-              title: Text(arrNames[index],style: Theme.of(context).textTheme.headlineLarge!.copyWith(color: Colors.blue)),
-              subtitle: Text('Delhi',style: basicTextStyle(),),
-              trailing: Icon(Icons.add) ,
             ),
-          ),
-        );
-      },
+          );
+        },
         itemCount: arrNames.length,
         reverse: false,
-        scrollDirection: Axis.vertical, separatorBuilder: (BuildContext context, int index) {
-        return Divider(height: 10,thickness: 3,);
+        scrollDirection: Axis.vertical,
+        separatorBuilder: (BuildContext context, int index) {
+          return Divider(height: 10, thickness: 3,);
         },
       ),
     );
